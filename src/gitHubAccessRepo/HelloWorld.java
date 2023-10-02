f622d29bf292307cc3ac8390aa9e8d2734869e25
bsayre2 <bsayre2@emich.edu>
2023-10-02 19:05:31 -0400

    Added alternate

---
+++ src/gitHubAccessRepo/HelloWorld.java
@@ -0,0 +1,12 @@
+package gitHubAccessRepo;
+
+public class HelloWorld {
+
+	public static void main (String[] args) {
+		System.out.println("Hi world");
+		System.out.println("this is my second alternate print");
+	}
+	
+	
+	
+}
